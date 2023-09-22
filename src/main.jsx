85acb1d890e3d5a0aecbbab459c7c8d9b2018bd3
sajjadsunnyofficial <sajjadhossainsunny.official@gmail.com>
2023-09-22 09:10:15 +0600

    add: added React Router Dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Register from './Pages/Register/Register'
+import Login from './Pages/Login/Login'
 import './index.css'
 import './firebaseConfig.jsx'
 import 'react-toastify/dist/ReactToastify.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/sign-up",
+    element: <Register></Register>
+  },
+  {
+    path: "/sign-in",
+    element: <Login></Login>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
