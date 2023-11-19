daed391c9b6acd7d6ab82446fd446a5ff4bf9545
sajjadsunnyofficial <sajjadhossainsunny.official@gmail.com>
2023-11-19 22:26:49 +0600

    Add: block and unblock work completed

--- src/components/BlockList/BlockList.jsx
+++ src/components/BlockList/BlockList.jsx
@@ -1,6 +1,53 @@
 import { HiDotsVertical } from 'react-icons/hi';
+import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
+import { useEffect, useState } from 'react';
+import { useSelector } from "react-redux";
 
 const BlockList = () => {
+   const db = getDatabase();
+   const data = useSelector(state => state.userLoginInfo.userInfo);
+   const [blockList, setBlockList] = useState([]);
+
+   useEffect(() => {
+      const blockRef = ref(db, 'block/');
+      onValue(blockRef, (snapshot) => {
+         let arr = []
+         snapshot.forEach((item) => {
+            if (item.val().blockeduserid == data.uid || item.val().blockedbyid == data.uid) {
+               if (item.val().blockeduserid == data.uid) {
+                  arr.push({
+                     id: item.key,
+                     block: item.val().blockedbyname,
+                     blockedbyid: item.val().blockedbyid,
+                     blocked_dp: item.val().blockedbyphoto,
+                  })
+               } else {
+                  arr.push({
+                     id: item.key,
+                     block: item.val().blockedusername,
+                     blockedid: item.val().blockeduserid,
+                     blocked_dp: item.val().blockeduserphoto,
+                  })
+               }
+            }
+         })
+         setBlockList(arr)
+      });
+   }, [db, data])
+
+   const unblockUser = (item) => {
+      set(push(ref(db, 'friends/')), {
+         sendername: item.block,
+         senderid: item.blockedid,
+         receivername: data.displayName,
+         receiverid: data.uid,
+         sender_dp: item.blocked_dp,
+         receiver_dp: data.photoURL,
+      }).then(() => {
+         remove(ref(db, 'block/' + item.id))
+      })
+   }
+
    return (
       <>
          <div className="w-full h-full pt-5 pb-3 pl-5 pr-[22px] rounded-custom shadow-homeCardShadow">
@@ -12,20 +59,32 @@ const BlockList = () => {
             </div>
             <ul className='eraseBorder h-[86%] overflow-y-auto'>
 
-               <li className='py-3 flex justify-between items-center border-b-[1px] border-solid border-[#00000040]'>
-                  <div className="flex items-center">
-                     <div className="mr-3.5">
-                        <img className='w-[54px] h-[54px] rounded-full object-cover' src="images/friends/Ellipse2.png" alt="Ellipse2.png" />
-                     </div>
-                     <div className=''>
-                        <h5 className='font-poppins text-sm font-semibold'>Raghav</h5>
-                        <h5 className="font-poppins text-[10px] font-medium text-[#00000080] mt-1">Today, 8:56pm</h5>
-                     </div>
-                  </div>
-                  <div className="mr-0 md:mr-6">
-                     <button className='font-poppins text-sm md:text-lg font-semibold text-white px-2.5 py-0.5 bg-themeColor rounded-md border-[1px] border-solid border-themeColor hover:bg-white hover:text-themeColor duration-300'>Unblock</button>
-                  </div>
-               </li>
+               {
+                  blockList.map((item, i) => (
+                     <li key={i} className='py-3 flex justify-between items-center border-b-[1px] border-solid border-[#00000040]'>
+                        <div className="flex items-center">
+                           <div className="mr-3.5">
+                              <img className='w-[54px] h-[54px] rounded-full  object-cover' src={item.blocked_dp} alt="Ellipse2.png" />
+                           </div>
+                           <div className=''>
+                              <h5 className='font-poppins text-sm font-semibold'>{item.block}</h5>
+                              <h5 className="font-poppins text-[10px] font-medium text-[#00000080] mt-1">Today, 8:56pm</h5>
+                           </div>
+                        </div>
+                        <div className="">
+                           {
+                              !item.blockedbyid ?
+                                 <button onClick={() => unblockUser(item)} className='font-poppins text-sm md:text-lg font-semibold text-white px-2.5 py-0.5 bg-themeColor rounded-md border-[1px] border-solid border-themeColor hover:bg-white hover:text-themeColor duration-300'>Unblock</button>
+                                 :
+                                 <h5 className="font-poppins text-[10px] font-medium text-[#00000080] select-none">User blocked you</h5>
+                           }
+
+                        </div>
+                     </li>
+                  ))
+               }
+
+
 
             </ul>
          </div>
--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -21,6 +21,33 @@ const Friends = () => {
       });
    }, [db, data])
 
+   const blockUser = (item) => {
+      if (data.uid == item.senderid) {
+         set(push(ref(db, 'block/')), {
+            blockeduserid: item.receiverid,
+            blockedusername: item.receivername,
+            blockeduserphoto: item.receiver_dp,
+            blockedbyid: item.senderid,
+            blockedbyname: item.sendername,
+            blockedbyphoto: item.sender_dp,
+         }).then(() => {
+            remove(ref(db, 'friends/' + item.key))
+         })
+      } else if (data.uid == item.receiverid) {
+         set(push(ref(db, 'block/')), {
+            blockeduserid: item.senderid,
+            blockedusername: item.sendername,
+            blockeduserphoto: item.sender_dp,
+            blockedbyid: item.receiverid,
+            blockedbyname: item.receivername,
+            blockedbyphoto: item.receiver_dp,
+         }).then(() => {
+            remove(ref(db, 'friends/' + item.key))
+         })
+      }
+
+   }
+
    return (
       <>
          <div className="w-full h-full pt-5 pb-3 pl-5 pr-[22px] rounded-custom shadow-homeCardShadow">
@@ -36,7 +63,7 @@ const Friends = () => {
                      <li key={i} className='py-3 flex justify-between items-center border-b-[1px] border-solid border-[#00000040]'>
                         <div className="flex items-center">
                            <div className="relative mr-3.5 after:content-[''] after:h-[15px] after:w-[15px] after:bg-[#00FF75] after:absolute after:bottom-0 after:right-0 after:rounded-full after:border-solid after:border-white after:border-2 after:drop-shadow-navIconDropShadow">
-                              <img className='w-[54px] h-[54px] rounded-full object-cover' src={data.uid == item.senderid ? item.receiver_dp : item.sender_dp} alt="" />
+                              <img className='w-[54px] h-[54px] bg-slate-100 rounded-full object-cover' src={data.uid == item.senderid ? item.receiver_dp : item.sender_dp} alt="" />
                            </div>
                            <div className=''>
                               <h5 className='font-poppins text-sm font-semibold'>
@@ -48,7 +75,7 @@ const Friends = () => {
                            </div>
                         </div>
                         <div className="">
-                           <button className='font-poppins text-sm md:text-lg font-semibold text-white px-2.5 py-0.5 bg-themeColor rounded-md border-[1px] border-solid border-themeColor hover:bg-white hover:text-themeColor duration-300'>Block</button>
+                           <button onClick={() => blockUser(item)} className='font-poppins text-sm md:text-lg font-semibold text-white px-2.5 py-0.5 bg-themeColor rounded-md border-[1px] border-solid border-themeColor hover:bg-white hover:text-themeColor duration-300'>Block</button>
                         </div>
                      </li>
                   ))
